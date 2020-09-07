47d2cb7aa334aa04c9ceb4fff8421e0966efc559
srx <strax1990@gmail.com>
2020-09-07 12:38:26 +0500

    resolve board/cards/creator fields in lists-resolver

--- back/src/modules/lists/lists.module.ts
+++ back/src/modules/lists/lists.module.ts
@@ -5,10 +5,14 @@ import { ListSchema, ListDocument } from './schemas/list.schema';
 import { BoardsModule } from '@/modules/boards/boards.module';
 import { ListsResolver } from './resolvers/lists.resolver';
 import { ListsMutations } from './mutations/lists.mutations';
+import { CardsModule } from '../cards/cards.module';
+import { UsersModule } from '../users/users.module';
 
 @Module({
   imports: [
     forwardRef(() => BoardsModule),
+    forwardRef(() => CardsModule),
+    forwardRef(() => UsersModule),
     MongooseModule.forFeature([
       {
         name: ListDocument.name,
--- back/src/modules/lists/resolvers/lists.resolver.ts
+++ back/src/modules/lists/resolvers/lists.resolver.ts
@@ -1,16 +1,39 @@
-import { Resolver, Args, Query } from '@nestjs/graphql';
+import { Resolver, Args, Query, ResolveField, Parent } from '@nestjs/graphql';
 import { List } from '../schemas/list.graphql.schema';
 import { ListsService } from '../lists.service';
+import { Card } from '@/modules/cards/schemas/card.graphql.schema';
+import { CardsService } from '@/modules/cards/cards.service';
+import { User } from '@/modules/users/schemas/user.graphql.schema';
+import { UsersService } from '@/modules/users/users.service';
+import { Board } from '@/modules/boards/schemas/board.graphql.schema';
+import { BoardsService } from '@/modules/boards/boards.service';
 
 @Resolver(() => List)
 export class ListsResolver {
-  constructor(private listsService: ListsService) {}
-  @Query(returns => List, { name: 'list' })
+  constructor(
+    private listsService: ListsService,
+    private cardsService: CardsService,
+    private usersService: UsersService,
+    private boardsService: BoardsService,
+  ) {}
+  @Query(() => List, { name: 'list' })
   async getList(@Args('id') id: string): Promise<List> {
     return this.listsService.findById(id);
   }
-  @Query(returns => [List], { name: 'lists' })
+  @Query(() => [List], { name: 'lists' })
   async getLists(): Promise<List[]> {
     return this.listsService.findMany();
+  }
+  @ResolveField('board', () => Board)
+  async getBoard(@Parent() list: List) {
+    return this.boardsService.findById(list.board);
+  }
+  @ResolveField('cards', () => Card)
+  async getCards(@Parent() list: List) {
+    return this.cardsService.findMany({ list: list._id });
+  }
+  @ResolveField('creator', () => User)
+  async getCreator(@Parent() list: List) {
+    return this.usersService.findById(list.creator);
   }
 }
